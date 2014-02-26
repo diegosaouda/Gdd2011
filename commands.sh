ba7422308399aba5bc0332ea935dfe2fe2a5dbb9
Diego Saouda <diegosaouda@gmail.com>
2014-02-26 17:34:23 -0300

    Commands

--- commands.sh
+++ commands.sh
@@ -12,3 +12,5 @@ vendor/bin/phpcpd src/
 #Uso do phpcs (validar a qualidade do codigo escrito segundo a PSR)
 #Eh preciso instalar o php_CodeSniffer
 phpcs --standard=PSR2 src/
+
+php php-cs-fixer.phar fix src/ --fixers=linefeed,short_tag,indentation,phpdoc_params,unused_use,trailing_spaces,return,visibility,php_closing_tag,braces,extra_empty_lines,function_declaration,include,controls_spaces,psr0,elseif,eof_ending

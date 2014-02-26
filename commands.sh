84a12502a3a308e4ed233747fb13fb88193ddc02
Diego Saouda <diegosaouda@gmail.com>
2014-02-26 15:55:53 -0300

    Adicionado outras ferramentas para qualidade de codigo

--- commands.sh
+++ commands.sh
@@ -6,6 +6,9 @@ vendor/bin/php-cs-fixer fix src/ -vv
 #uso do phpmd (mess detector)
 vendor/bin/phpmd src/ text codesize,controversial,design,naming,unusedcode
 
+#Verificar codigos duplicados (copy paste)
+vendor/bin/phpcpd src/
+
 #Uso do phpcs (validar a qualidade do codigo escrito segundo a PSR)
 #Eh preciso instalar o php_CodeSniffer
 phpcs --standard=PSR2 src/
